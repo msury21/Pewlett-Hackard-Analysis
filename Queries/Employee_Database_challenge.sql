1e625a6b6a39a3811c078490e21e18b0e35658df
msury21 <meenakshi.a.suryanarayanan@gmail.com>
2022-10-27 22:54:13 -0500

    deliverable 2

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -53,3 +53,44 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY COUNT(title) desc;
+
+-- DELIVERABLE 2!!!!
+
+-- 4. Use a DISTINCT ON statement to retrieve the
+-- first occurrence of the employee number for each
+-- set of rows defined by the ON () clause.
+SELECT DISTINCT ON (e.emp_no)
+-- 1. Retrieve the emp_no, first_name, last_name,
+-- and birth_date columns from the Employees table.
+				   e.emp_no,
+				   e.first_name,
+				   e.last_name,
+				   e.birth_date,
+-- 2. Retrieve the from_date and to_date columns
+-- from the Department Employee table.
+				   de.from_date,
+				   de.to_date,
+-- 3. Retrieve the title column from the Titles table.
+				   ti.title
+-- 5. Create a new table using the INTO clause.
+INTO mentorship_eligibilty
+FROM employees as e
+-- 6. Join the Employees and the Department
+-- Employee tables on the primary key.
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+-- 7. Join the Employees and the Titles
+-- tables on the primary key.
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+-- 8. Filter the data on the to_date column to all
+-- the current employees, then filter the data on
+-- the birth_date columns to get all the employees
+-- whose birth dates are between January 1, 1965
+-- and December 31, 1965.
+WHERE (de.to_date = '9999-01-01')
+  AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+-- 9. Order the table by the employee number.
+ORDER BY e.emp_no
+				   
+	   
